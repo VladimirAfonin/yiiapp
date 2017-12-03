1d08d441bafa0984f60973629bed4182cb9203a1
vlafo <afonin006@gmail.com>
2017-12-03 19:51:43 +0300

    - category/index

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -3,6 +3,8 @@
 namespace app\controllers;
 
 use app\models\Products;
+use app\models\Categories;
+use Yii;
 
 class CategoryController extends AppController
 {
@@ -17,5 +19,20 @@ class CategoryController extends AppController
         $hitProducts = Products::find()->where(['hit' => '1'])->asArray()->limit(6)->all();
 
         return $this->render('index', compact('hitProducts'));
+    }
+
+    /**
+     * получаем продукты по выбранной категории
+     *
+     * @param $id
+     * @return string
+     */
+    public function actionView($id)
+    {
+        // $_GET['id']
+        $categoryId = Yii::$app->request->get('id');
+        $products = Products::find()->where(['category_id' => $categoryId])->asArray()->all();
+
+        return $this->render('view', compact('products'));
     }
 }
